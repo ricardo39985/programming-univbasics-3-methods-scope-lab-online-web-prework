8e80f7308b8eef7fe2b91376266c0dded1b8f586
Ricardo Persaud <ricardo39985@gmail.com>
2019-11-07 00:51:22 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,13 +1,7 @@
-@status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 def mario
-  describe "mario" do
-  it "puts out 'It's-a me, Mario!'" do
-    phrase = "It's-a me, Mario!"
-    expect{mario}.to output("It's-a me, Mario!\n").to_stdout
-  end
-end
-
-  puts phrase
+  phrase = "It's-a me, Mario!"
+  puts
 end
 
 def toadstool
